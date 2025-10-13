99e48dc36c2b87e73c090fc325fe4708b327a65a
Fachry Anwar <fachryanwar2926@gmail.com>
2025-10-13 10:07:17 +0700

    Initial Commit

---
+++ selakata/selakataApp.swift
@@ -0,0 +1,17 @@
+//
+//  selakataApp.swift
+//  selakata
+//
+//  Created by Fachry Anwar on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct selakataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
