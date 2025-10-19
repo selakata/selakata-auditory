c090189e4125b7d02171f36898e89a379dd5a2c3
anisamlia <anisamlia5581@gmail.com>
2025-10-19 20:20:20 +0700

    feat: modules

---
+++ selakata/Views/ModulesView.swift
@@ -0,0 +1,56 @@
+//
+//  ModulesView.swift
+//  selakata
+//
+//  Created by Anisa Amalia on 18/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ModulesView: View {
+    @StateObject private var viewModel = ModulesViewModel()
+    
+    @Query(sort: \Module.orderIndex) private var allModules: [Module]
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Exercise")
+                        .font(.largeTitle.weight(.bold))
+                        .padding([.horizontal, .top])
+                    
+                    ForEach(allModules) { module in
+                        let isUnlocked = viewModel.isModuleUnlocked(module, in: allModules)
+                        
+                        NavigationLink(destination: Text("Module Detail Page")) {
+                            ModuleCard(module: module, showProgressBar: false)
+                                .overlay(
+                                    isUnlocked ? nil : LockOverlay()
+                                )
+                        }
+                        .buttonStyle(.plain)
+                        .disabled(!isUnlocked)
+                    }
+                    .padding(.horizontal, 24)
+                }
+            }
+            .navigationTitle("Exercise")
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct LockOverlay: View {
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.5)
+            
+            Image(systemName: "lock.fill")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+        }
+        .cornerRadius(20)
+    }
+}
