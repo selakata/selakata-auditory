8c38184a84a5bd4fd42a00d47d933078433e8407
Rais Zainuri <raiszainuri09@gmail.com>
2025-11-18 09:18:17 +0700

    feat: onbosrding quizz

---
+++ selakata/Presentation/Quiz/OnBoardingQuizView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingQuizView.swift
+//  selakata
+//
+//  Created by ais on 17/11/25.
+//
+
+import SwiftUI
+
+struct OnBoardingQuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingQuizView()
+}
